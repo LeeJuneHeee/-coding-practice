d8aeabc4e108d6303b20aabfafef9203ca83ac5c
LeeJuneHee <leejuneheenaver@gmail.com>
2026-06-14 08:50:34 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    odd = 0
+    even = 0
+    
+    for i in range(len(num_list)):
+        if i % 2 == 1:
+            odd += num_list[i]
+        else:
+            even += num_list[i]
+    
+    return max(odd, even)
