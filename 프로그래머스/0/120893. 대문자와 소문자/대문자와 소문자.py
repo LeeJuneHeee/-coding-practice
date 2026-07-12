c56960e3b7d0b1418f52ba08bc29363f91d8acd7
LeeJuneHee <leejuneheenaver@gmail.com>
2026-07-12 17:18:59 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.12 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    result = ''
+    
+    for char in my_string:
+        if char.islower():
+            result += char.upper()
+        else:
+            result += char.lower()
+            
+    return result
